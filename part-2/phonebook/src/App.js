2069b53467b7b31cd4bc1279a52c969d44e2be4a
Ermias Hailemicheal <ermias.hailemicheal@frantic.com>
2020-05-08 23:12:55 +0300

    Adds final parts of phonebook task

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
+import personsService from './services/persons';
+import './App.css';
 
 const Input = ({ fieldName, name, handler }) => (
   <div>
@@ -27,64 +29,137 @@ const Form = ({
   </form>
 );
 
-const Persons = ({ personsList }) =>
-  personsList.map(({ name, number }) => (
-    <p key={name}>
+const Persons = ({ personsList, deleteHandler }) =>
+  personsList.map(({ name, number, id }) => (
+    <div key={id}>
       {name} {number}
-    </p>
+      <button
+        style={{ backgroundColor: 'red', marginLeft: '1rem' }}
+        onClick={() => deleteHandler(id)}
+      >
+        delete
+      </button>
+    </div>
   ));
+
+const Notification = ({ message, type }) => {
+  if (!message) return null;
+
+  return (
+    <div className={`message ${type === 'error' ? 'error' : 'success'}`}>
+      {message}
+    </div>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [errorType, setNotificationType] = useState('success');
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3001/persons`)
-      .then(({ data }) => setPersons(data));
+    personsService
+      .getAll()
+      .then(({ data }) => setPersons(data))
+      .catch((e) => updateErrorMessage('Server down!'));
   }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const updateSuccessState = (message, newValues) => {
+    setNotificationType('success');
+    setNotificationMessage(`${message} ${newName}`);
+    setTimeout(() => setNotificationMessage(null), 3000);
 
-    const hasIndex = persons.findIndex((v) => v.name === newName) > -1;
-
-    if (hasIndex) {
-      alert(`${newName} is already added to phonebook`);
-      setNewName('');
-      return;
-    }
-
-    setPersons([
-      ...persons,
-      {
-        name: newName,
-        number: newNumber,
-      },
-    ]);
+    setPersons(newValues);
     setNewName('');
     setNewNumber('');
   };
 
+  const updateErrorMessage = (message) => {
+    setNotificationType('error');
+    setNotificationMessage(message);
+    setTimeout(() => setNotificationMessage(null), 3000);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newData = {
+      name: newName,
+      number: newNumber,
+    };
+
+    const hasIndex = persons.findIndex((v) => v.name === newName) > -1;
+
+    if (hasIndex) {
+      const { id } = persons.filter((v) => v.name === newName)[0];
+
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with the new one?`
+        )
+      ) {
+        personsService
+          .update(id, newData)
+          .then((response) => {
+            const filterPerson = persons.map((person) =>
+              person.id !== id ? person : response.data
+            );
+
+            updateSuccessState('Updated', filterPerson);
+          })
+          .catch((error) => {
+            updateErrorMessage(
+              `Information of ${newName} has already been removed from the server.`
+            );
+          });
+        return;
+      }
+    }
+
+    personsService
+      .create(newData)
+      .then((response) => {
+        updateSuccessState('Added', [...persons, response.data]);
+      })
+      .catch((error) => {
+        updateErrorMessage(`Error! Can't add!`);
+      });
+  };
+
   const handleNameChange = ({ target }) => setNewName(target.value);
   const handleNumberChange = ({ target }) => setNewNumber(target.value);
-  const handleSearch = ({ target }) => {
-    const currentSearch = target.value;
-    setSearchTerm(currentSearch);
-  };
+  const handleSearch = ({ target }) => setSearchTerm(target.value);
 
   const filterList = (list) =>
     list.filter(({ name }) => {
       return name.toLocaleLowerCase().includes(searchTerm.toLowerCase());
     });
 
+  const deleteHandler = (id) => {
+    const { name } = persons.filter((person) => person.id === id)[0];
+
+    if (window.confirm(`Delete ${name}?`)) {
+      personsService
+        .deleteItem(id)
+        .then((response) => {
+          setPersons(persons.filter((person) => person.id !== id));
+          updateErrorMessage(`Deleted ${name}`);
+        })
+        .catch((error) => {
+          updateErrorMessage(`${name} has been already deleted!`);
+        });
+    }
+  };
+
   const personsList = !searchTerm ? persons : filterList(persons);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} type={errorType} />
       <Filter name={searchTerm} handler={handleSearch} />
 
       <h2>Add a new</h2>
@@ -97,7 +172,7 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Persons personsList={personsList} />
+      <Persons personsList={personsList} deleteHandler={deleteHandler} />
     </div>
   );
 };
